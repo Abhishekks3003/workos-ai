a5746594c56817d70c1f9977680f337718e7a38e
Abhishek <2029156@kiit.ac.in>
2026-06-20 16:51:46 +0530

    Initial project setup

---
+++ backend/src/main/java/com/abhishek/workos/WorkosApplication.java
@@ -0,0 +1,13 @@
+package com.abhishek.workos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkosApplication.class, args);
+	}
+
+}
